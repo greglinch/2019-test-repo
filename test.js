313c742cc553d0bb38ae890378673ec9d64958fe
Greg Linch <greglinch@gmail.com>
2019-02-03 15:36:57 -0500

    Revert "Revert "add JS file with pseudocode""
    This reverts commit 5cb491bc8fb66d66d5b32ca52e4978928df41cc9.

---
+++ test.js
@@ -0,0 +1,57 @@
+// this is a single-line JavaScript comment
+
+/*
+this is a multi-line JavaScript
+*/
+
+// when the document is ready... (a.k.a. wait until the HTML has loaded)
+
+	// define your variables
+
+	// custom functions
+
+		// variables
+
+	// if this condition is true...
+
+		// do something or call a function
+
+	// or else do this
+
+		// another set of instructions to follow or function to call
+
+
+
+
+
+
+
+
+
+		// step 1
+
+
+		// step 2
+
+
+		// step 3
+
+
+		// step 4
+
+
+		// step 5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
